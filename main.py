92c2aaec5c1312491c2cfffb555c79c967ad404d
Partsey <partsey2412@gmail.com>
2018-09-24 10:21:33 +0300

    display some information about triangulation

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ class Application:
 
         self.text_frame = Tk.Frame(master=self.root)
         self.text = Tk.Text(self.text_frame)
-        self.text.
         self.text_scrollbar = Tk.Scrollbar(self.text_frame)
         self.text.config(yscrollcommand=self.text_scrollbar.set)
         self.text.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)
@@ -64,7 +63,7 @@ class Application:
         self.angle = None
         self.area = None
         self.points = None
-        self.segments = None
+        self.boundary_segments = None
         self.data = None
         self.holes = None
 
@@ -73,35 +72,44 @@ class Application:
             return json.load(f)
 
     def load_data(self):
+        # open file dialog
         self.filepath = Tk.filedialog.askopenfilename(title="Select file", filetypes=(("json files", "*.json"), ("all files", "*.*")))
         if self.filepath is not None:
-            self.fig.clear()
+            self.fig.clear()  # clear everything that was drawn before
 
+            # read data
             self.data = self.read_data()
             self.angle = self.data.get('angle')
+            self.angle = 0 if self.angle > 17.5 else self.angle  # check if angle is bad for triangulation algorithm
             self.area = self.data.get('area')
             self.holes = self.data.get('holes')
             self.points = self.data['points']
 
+            # display data in the text field
+            self._print_info(self.data)
 
-            self.text.delete('1.0', Tk.END)
-            pretty_data = pprint.pformat(self.data)
-            self.text.insert(Tk.END, pretty_data)
 
-            self.segments = [[i, (i+1) % len(self.points)] for i in range(len(self.points))]
+            # get boundary elements
+            self.boundary_segments = [[i, (i+1) % len(self.points)] for i in range(len(self.points))]
 
+            # declare dict that 'triangle' library require for triangulation
             self.A = {
                 'vertices': np.array([(point['x'], point['y']) for point in self.data['points']]),
-                'segments': np.array(self.segments)
+                'segments': np.array(self.boundary_segments)
             }
 
+            # if holes are specified add them to dict
             if self.holes:
                 self.A['holes'] = np.array(self.holes)
 
-
-
+            # plot the domain
             plot.plot(plt, self.A)
-            self.canvas.draw()
+            self.canvas.draw() # redraw the canvas
+
+    def _print_info(self, data):
+        self.text.delete('1.0', Tk.END)
+        pretty_data = pprint.pformat(data)  # pretty format data before printing
+        self.text.insert(Tk.END, pretty_data)
 
     def _quit(self):
         self.root.quit()     # stops mainloop
@@ -109,16 +117,19 @@ class Application:
                         # Fatal Python Error: PyEval_RestoreThread: NULL tstate
 
     def triangulate(self):
+        # triangulate domain
+
         self.B = triangle.triangulate(
             self.A,
             f'pq{self.angle if self.angle else ""}a{self.area if self.area else ""}')
+        b_info = {key: value.tolist() if isinstance(value, np.ndarray) else value for key,value in self.B.items()}
+        self._print_info(json.dumps(b_info))
         plot.plot(plt, self.B)
         self.canvas.draw()
 
     def compare(self):
         plot.compare(plt, self.A, self.B)
         self.canvas.draw()
-
 
     def run(self):
         Tk.mainloop()
