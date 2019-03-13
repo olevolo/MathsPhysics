fd8fa5b8a8b51aa4714ccd944ef1239c4eaf2a1b
Volodymyr Oleksiuk <olevolo@gmail.com>
2019-03-13 20:33:37 +0200

    corrected l2 errors

--- AppGUI.py
+++ AppGUI.py
@@ -96,13 +96,26 @@ class AppGUI:
         self.solution = np.linalg.solve(self.solver.K, self.solver.F)
         print('-' * 100)
         print("FEM solution : ")
-        pprint.pprint(self.solution)
+        print(self.solution)
 
         self.tester = Tester(self.solution, self.entries, self.delaunay_triangulation, self.solver)
+        print("Exact solution : ")
+        pprint.pprint(self.tester.get_exact_soluton())
+        print()
+        print("n: ", self.tester.n)
         print("L2:")
-        print("absolute error: ", self.tester.get_abs_error_L2())
-        print("relative error: ", self.tester.get_relative_error_L2())
+        print("absolute error: ")
+        print(np.sum(self.tester.get_abs_error_L2()) / self.tester.n)
+        #pprint.pprint(self.tester.get_abs_error_L2())
+        print("relative error: ")
+        pprint.pprint(self.tester.get_relative_error_L2())
+
         self.tester.derivative()
+        print("W2:")
+        print("absolute error: ")
+        #print(np.sum(self.tester.get_abs_error_W2()) / self.tester.n)
+        print("relative error: ")
+        #pprint.pprint(self.tester.get_relative_error_W2())
 
 
         filename = 'matrices.json'
@@ -183,11 +196,11 @@ class AppGUI:
         a_row = Tk.Frame(self.cond_window)
         Tk.Label(a_row, width=22, text='a11 :', anchor='w').pack(side=Tk.LEFT)
         a11_ent = Tk.Entry(a_row)
-        a11_ent.insert(0, "1")
+        a11_ent.insert(0, "4")
         a11_ent.pack(side=Tk.LEFT, expand=Tk.YES, fill=Tk.X)
         Tk.Label(a_row, width=22, text='a22 :', anchor='w').pack(side=Tk.LEFT)
         a22_ent = Tk.Entry(a_row)
-        a22_ent.insert(0, "1")
+        a22_ent.insert(0, "3")
         a22_ent.pack(side=Tk.LEFT, expand=Tk.YES, fill=Tk.X)
         a_row.pack(side=Tk.TOP, fill=Tk.X, padx=5, pady=5)
 
@@ -197,11 +210,11 @@ class AppGUI:
         fd_row = Tk.Frame(self.cond_window)
         Tk.Label(fd_row, width=22, text='f :', anchor='w').pack(side=Tk.LEFT)
         f_ent = Tk.Entry(fd_row)
-        f_ent.insert(0, "1")
+        f_ent.insert(0, "7")
         f_ent.pack(side=Tk.LEFT, expand=Tk.YES, fill=Tk.X)
         Tk.Label(fd_row, width=22, text='d :', anchor='w').pack(side=Tk.LEFT)
         d_ent = Tk.Entry(fd_row)
-        d_ent.insert(0, "1")
+        d_ent.insert(0, "0")
         d_ent.pack(side=Tk.LEFT, expand=Tk.YES, fill=Tk.X)
         fd_row.pack(side=Tk.TOP, fill=Tk.X, padx=5, pady=5)
 
@@ -224,7 +237,7 @@ class AppGUI:
             params = {}
             for key in fields:
                 ent = Tk.Entry(row)
-                ent.insert(0, "1")
+                ent.insert(0, "0.00001")
                 ent.pack(side=Tk.LEFT, expand=Tk.YES, fill=Tk.X)
                 params[key] = ent
 
--- tester.py
+++ tester.py
@@ -12,12 +12,11 @@ class Tester:
         self.a = 2
         self.n = len(delaunay_triangulation.triangulated_info['CT'])
         self.exact_solution = self.calculate_exact_solution(self.vertices)
+        self.exact_derivatives = self.calculate_exact_derivatives(self.vertices)
         self.delaunay_triangulation = delaunay_triangulation
-        self.vertex_deerivatives = {}
+        self.vertex_derivatives = {}
         self.derivatives = np.zeros_like(self.fem_solution)
         self.fem_solver = fem_solver
-
-
 
     def calculate_exact_solution(self, vertices):
         # hardcoded case from example
@@ -26,21 +25,32 @@ class Tester:
             exact_solution.append(self.conds['f'] / self.conds['a11'] * vertices[i][0] / 2 * (self.a - vertices[i][0]))
         return exact_solution
 
-    def get_abs_error_L2(self):
-        print(len(self.exact_solution), " ", len(self.fem_solution), " ", self.n)
-        return [np.abs(np.power(self.exact_solution[i] - self.fem_solution[i], 2)) for i in range(0, self.n)]
-        #return np.sum(np.abs(np.power(self.exact_solution - self.fem_solution, 2)))
+    def get_exact_soluton(self):
+        return self.exact_solution
 
+    def get_abs_error_L2(self):
+        # print(len(self.exact_solution), " ", len(self.fem_solution), " ", self.n)
+        return [np.power(np.abs(self.exact_solution[i] - self.fem_solution[i]), 2) for i in range(0, self.n)]
+        # return np.sum(np.abs(np.power(self.exact_solution - self.fem_solution, 2)))
 
     def get_relative_error_L2(self):
-        #return self.get_abs_error_L2() / np.sum(np.abs(np.power(self.fem_solution,2)))
+        # return self.get_abs_error_L2() / np.sum(np.abs(np.power(self.fem_solution,2)))
         return np.sum(self.get_abs_error_L2()) / np.sum(np.abs(np.power(self.fem_solution, 2)))
 
     def get_abs_error_W2(self):
-        return None
+        print(self.derivatives)
+        return [self.get_abs_error_L2()[i] + np.power(np.abs(self.exact_derivatives[i] - self.derivatives[i]), 2)
+                for i in range(0, self.n)]
 
     def get_relative_error_W2(self):
-        return None
+        return np.sum(self.get_abs_error_W2()) / ( np.sum(np.abs(np.power(self.fem_solution, 2))) +
+                                                         np.sum(np.abs(np.power(self.derivatives, 2))) )
+
+    def calculate_exact_derivatives(self, vertices):
+        exact_derivatives = []
+        for i in range(len(vertices)):
+            exact_derivatives.append(self.conds['f'] / self.conds['a11'] * vertices[i][0] / 2 * (self.a - vertices[i][0]))
+        return exact_derivatives
 
     def derivative(self):
         for triangle in self.delaunay_triangulation.triangulated['triangles']:
@@ -53,11 +63,11 @@ class Tester:
                 derivative += (b + c)*u_h[i, 0]
             derivative /= delta
             for point_idx in triangle:
-                if str(point_idx) in self.vertex_deerivatives:
-                    self.vertex_deerivatives[str(point_idx)].append(derivative)
+                if str(point_idx) in self.vertex_derivatives:
+                    self.vertex_derivatives[str(point_idx)].append(derivative)
                 else:
-                    self.vertex_deerivatives[str(point_idx)] = [derivative]
+                    self.vertex_derivatives[str(point_idx)] = [derivative]
 
-        for k, v in self.vertex_deerivatives.items():
+        for k, v in self.vertex_derivatives.items():
             self.derivatives[int(k), 0] = np.average(np.array(v))
 
